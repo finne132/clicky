4683bcf85316ac4e459494d014bc458822e9740c
finne132 <finne132@umn.edu>
2018-07-16 07:41:33 -0500

    changed class to className and added basic Header

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,8 +6,8 @@ import React from 'react';
 // create the footer component
 const Footer = () => (
 <footer>
-    <div class="container">
-      <span class="footer-text">UMN Coding Bootcamp React Homework 1</span>
+    <div className="container">
+      <span classNAme="footer-text">UMN Coding Bootcamp React Homework 1</span>
   </div>
 </footer>
 )
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => (
+  <div className="jumbotron p-3">
+    <h1 className="text-center">React.JS Clicky Game: Click an image to start</h1>
+    <h3 className="text-center">Click on an image to start</h3>
+  </div>
+)
+export default Header;
