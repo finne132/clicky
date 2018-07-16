ce4c5cc1d3f8f56bc7a98c144718252830f14167
finne132 <finne132@umn.edu>
2018-07-16 07:49:06 -0500

    added in basic navbar component with score display

---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+"use strict";
+
+import React from "react";
+
+const Nav = (props) => (
+  <nav className="navbar navbar-light bg-info">
+    <span className="navbar-brand h1">Clicky Game</span>
+    <span className="text-center h3">{props.message}</span>
+    <span className="navbar-text">
+      Your Current Score: <span>{props.correct}</span>&nbsp;
+      Your Best Score: <span>{props.topscore}</span>
+    </span>
+  </nav>
+)
+export default Nav;
