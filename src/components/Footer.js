759e510c01c127e2ad1deb5ad23b1003a8c9db30
finne132 <finne132@umn.edu>
2018-07-16 07:37:41 -0500

    added in a basic footer

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+"use strict";
+
+//import react
+import React from 'react';
+
+// create the footer component
+const Footer = () => (
+<footer>
+    <div class="container">
+      <span class="footer-text">UMN Coding Bootcamp React Homework 1</span>
+  </div>
+</footer>
+)
+export default Footer;
